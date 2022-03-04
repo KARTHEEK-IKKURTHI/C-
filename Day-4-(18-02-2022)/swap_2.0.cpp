a8d6af918e40a11cfca9b03dbdebbc21f01f3671
Kartheek_Ikkurthi <98144061+KARTHEEK-IKKURTHI@users.noreply.github.com>
2022-03-04 10:52:34 +0530

    Day-4-

---
+++ Day-4-(18-02-2022)/swap_2.0.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+//Code to make swap using call by referernce.
+void swap(int* n1,int* n2)
+{
+int temp;
+temp = *n1;
+*n1 = *n2;
+*n2 = temp;
+}
+int main(){
+	int a = 1,b = 2;
+	cout<<"Before Swapping"<<endl;
+	cout<<"a="<<a<<endl;
+	cout<<"b="<<b<<endl;
+	swap(a,b);
+	cout<<"After Swapping"<<endl;
+	cout<<"a="<<a<<endl;
+	cout<<"b="<<b<<endl;
+return 0;
+}
